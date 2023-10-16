50c4f7445b716e64edf3e4794bd3647aff2b475e
Ihor Haivan <haivani@mydigicode.com>
2023-10-16 22:07:59 +0200

    Small fix

--- streamingApp/src/helpers/validate_and_format.py
+++ streamingApp/src/helpers/validate_and_format.py
@@ -1,5 +1,5 @@
 from pyspark.sql.functions import col, trim
-from pyspark.sql.types import StructType, StructField
+from pyspark.sql.types import StructType, StructField, StringType
 import logging
 
 
@@ -19,21 +19,42 @@ def validate_and_format(df, expected_schema=None):
             missing_columns = expected_columns - actual_columns
             extra_columns = actual_columns - expected_columns
             if missing_columns:
-                raise ValueError(f"Missing columns in the dataframe: {', '.join(missing_columns)}")
+                raise ValueError(
+                    f"Missing columns in the dataframe: {', '.join(missing_columns)}"
+                )
             if extra_columns:
-                logging.warning(f"Extra columns in the dataframe: {', '.join(extra_columns)}")
+                logging.warning(
+                    f"Extra columns in the dataframe: {', '.join(extra_columns)}"
+                )
 
             # Data Type Validation
             for field in expected_schema:
-                assert isinstance(field, StructField), "Each field in the expected schema should be of type StructField"
+                assert isinstance(
+                    field, StructField
+                ), "Each field in the expected schema should be of type StructField"
                 actual_dtype = df.schema[field.name].dataType
                 expected_dtype = field.dataType
                 if actual_dtype != expected_dtype:
-                    raise TypeError(f"Column '{field.name}' has datatype {actual_dtype}, but {expected_dtype} was expected")
+                    raise TypeError(
+                        f"Column '{field.name}' has datatype {actual_dtype}, but {expected_dtype} was expected"
+                    )
 
         # Data formatting
-        for column_name in df.columns:
-            df = df.withColumn(column_name, trim(col(column_name)))
+        # Filter only string columns
+        string_columns = [
+            f.name for f in df.schema.fields if isinstance(f.dataType, StringType)
+        ]
+
+        # Apply trim to string columns using a list comprehension
+        trimmed_cols = [
+            trim(col(column_name)).alias(column_name)
+            if column_name in string_columns
+            else col(column_name)
+            for column_name in df.columns
+        ]
+
+        # Use select to apply all transformations
+        df = df.select(*trimmed_cols)
 
         logging.info("Data validated and formatted successfully.")
         return df
--- streamingApp/src/main/streaming_consumer_app.py
+++ streamingApp/src/main/streaming_consumer_app.py
@@ -8,51 +8,67 @@ from src.helpers.flatten_xml import flatten_xml
 
 
 class StreamingConsumerApp:
-
-    def __init__(self, config_path='configs/application.conf', log_path='logs/streaming_consumer_app.log'):
+    def __init__(
+        self,
+        config_path="configs/application.conf",
+        log_path="logs/streaming_consumer_app.log",
+    ):
         self.logger = self.setup_logging(log_path)
         self.spark = SparkSession.builder.appName("StreamingConsumerApp").getOrCreate()
         self.conf = ConfigFactory.parse_file(config_path)
 
-        self.bootstrap_servers = self.conf.get_string('kafka.bootstrap.servers', None)
-        self.kafka_topic = self.conf.get_string('kafka.topic', None)
-        self.starting_offsets = self.conf.get_string('kafka.startingOffsets', None)
-        self.hdfs_raw_zone_path = self.conf.get_string('hdfs.raw-zone-path', None)
-        self.checkpoint_location = self.conf.get_string('checkpoint.location', None)
+        self.bootstrap_servers = self.conf.get_string("kafka.bootstrap.servers", None)
+        self.kafka_topic = self.conf.get_string("kafka.topic", None)
+        self.starting_offsets = self.conf.get_string("kafka.startingOffsets", None)
+        self.hdfs_raw_zone_path = self.conf.get_string("hdfs.raw-zone-path", None)
+        self.checkpoint_location = self.conf.get_string("checkpoint.location", None)
 
         self.parse_xml_udf = udf(parse_xml, StringType())
 
-        if not all([self.bootstrap_servers, self.kafka_topic, self.starting_offsets, self.hdfs_raw_zone_path,
-                    self.checkpoint_location]):
+        if not all(
+            [
+                self.bootstrap_servers,
+                self.kafka_topic,
+                self.starting_offsets,
+                self.hdfs_raw_zone_path,
+                self.checkpoint_location,
+            ]
+        ):
             self.logger.error("Required configuration not found in config file!")
             raise ValueError("Missing configuration data")
 
     def setup_logging(self, log_path):
-        logging.basicConfig(filename=log_path,
-                            level=logging.DEBUG,
-                            format='%(asctime)s - %(levelname)s - %(message)s')
+        logging.basicConfig(
+            filename=log_path,
+            level=logging.DEBUG,
+            format="%(asctime)s - %(levelname)s - %(message)s",
+        )
         return logging.getLogger()
 
     def get_kafka_stream(self):
-        stream = self.spark.readStream.format("kafka") \
-            .option("kafka.bootstrap.servers", self.bootstrap_servers) \
-            .option("subscribe", self.kafka_topic) \
-            .option("startingOffsets", self.starting_offsets) \
+        stream = (
+            self.spark.readStream.format("kafka")
+            .option("kafka.bootstrap.servers", self.bootstrap_servers)
+            .option("subscribe", self.kafka_topic)
+            .option("startingOffsets", self.starting_offsets)
             .load()
+        )
 
         # Add a timestamp column
-        stream_with_timestamp = stream.withColumn("processing_time", current_timestamp())
+        if "processing_time" not in stream.columns:
+            stream = stream.withColumn("processing_time", current_timestamp())
 
-        return stream_with_timestamp
+        return stream
 
     def process_kafka_stream(self, kafka_df):
-        kafka_df_with_watermark = kafka_df \
-            .withWatermark("processing_time", "1 hour")
+        kafka_df_with_watermark = kafka_df.withWatermark("processing_time", "1 hour")
 
-        processed_df = kafka_df_with_watermark.selectExpr("CAST(value AS STRING)") \
-            .withColumn("xml_content", self.parse_xml_udf("value")) \
-            .select(flatten_xml("xml_content")) \
+        processed_df = (
+            kafka_df_with_watermark.selectExpr("CAST(value AS STRING)")
+            .withColumn("xml_content", self.parse_xml_udf("value"))
+            .select(flatten_xml("xml_content"))
             .dropDuplicates()
+        )
 
         return processed_df
 
@@ -62,20 +78,22 @@ class StreamingConsumerApp:
             processed_df = self.process_kafka_stream(kafka_df)
 
             # Extract year, month, day, hour from processing_time using built-in Spark functions
-            processed_df = (processed_df
-                            .withColumn("year", year("processing_time"))
-                            .withColumn("month", month("processing_time"))
-                            .withColumn("day", dayofmonth("processing_time"))
-                            .withColumn("hour", hour("processing_time")))
+            processed_df = (
+                processed_df.withColumn("year", year("processing_time"))
+                .withColumn("month", month("processing_time"))
+                .withColumn("day", dayofmonth("processing_time"))
+                .withColumn("hour", hour("processing_time"))
+            )
 
             # Streaming to HDFS with checkpointing for fault tolerance
-            query = (processed_df.writeStream
-                     .format("parquet")
-                     .outputMode("overwrite")
-                     .option("path", self.hdfs_raw_zone_path)
-                     .option("checkpointLocation", self.checkpoint_location)
-                     .partitionBy("year", "month", "day", "hour")
-                     .start())
+            query = (
+                processed_df.writeStream.format("parquet")
+                .outputMode("overwrite")
+                .option("path", self.hdfs_raw_zone_path)
+                .option("checkpointLocation", self.checkpoint_location)
+                .partitionBy("year", "month", "day", "hour")
+                .start()
+            )
 
             query.awaitTermination()
             self.logger.info("Data from Kafka written to HDFS RAW zone successfully!")
